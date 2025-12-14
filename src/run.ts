29b7dc440c2897e63a40dab39d7ed4ff712f075e
dbrehov <brehov@gmail.com>
2025-12-14 10:29:00 -0800

    ok

--- src/run.ts
+++ src/run.ts
@@ -167,6 +167,10 @@ async function run(headless: boolean = true) {
                 await page.locator('button.bit-button is-round \\!text-content-tertiary').click();
 
                 await new Promise(resolve => setTimeout(resolve, 5000)); // задержка после клика
+                // Press enter
+                await page.keyboard.press("Tab"); // Press enter
+                await page.keyboard.press("Space");
+                await page.keyboard.press("Enter");
             } catch (err) {
                 console.log(`Кнопка следующей страницы недоступна:`, err);
             }
