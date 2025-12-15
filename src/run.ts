3fc74490962139e32b4983b72e19ea031d2f17ed
dbrehov <brehov@gmail.com>
2025-12-15 04:49:30 -0800

    ok

--- src/run.ts
+++ src/run.ts
@@ -401,7 +401,7 @@ async function run(headless: boolean = true) {
                 const pnlIndex = lines.findIndex(line => line === 'Ордер №');
                 let valueLine = 'NOT_FOUND';
                 if (pnlIndex > 0) {
-                    valueLine = lines[pnlIndex + 2] +  lines[pnlIndex + 3] +  lines[pnlIndex + 4] +  lines[pnlIndex + 5] +  lines[pnlIndex + 6];
+                    valueLine = lines[pnlIndex + 1] +  lines[pnlIndex + 2] +  lines[pnlIndex + 3] +  lines[pnlIndex + 4]  +  lines[pnlIndex + 5] +  lines[pnlIndex + 6] +  lines[pnlIndex + 7] +  lines[pnlIndex + 8]  +  lines[pnlIndex + 9] +  lines[pnlIndex + 10];
                 }
 
                 console.log(valueLine);
